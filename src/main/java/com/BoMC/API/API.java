37eee61647dd76b39b0df888c9d4be6afb93f879
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2021-06-12 23:01:30 -0600

    added load message aMaZiNg

--- src/main/java/com/BoMC/API/API.java
+++ src/main/java/com/BoMC/API/API.java
@@ -1,12 +1,15 @@
 package com.BoMC.API;
 
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class API extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		Bukkit.getLogger().log(Level.INFO, "PLUGIN LOADED");
 		
 		
 	}
