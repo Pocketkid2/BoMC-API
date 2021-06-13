bf046fb5712ee48afb05980c542928d22cdec732
Jacob Batista <jacobaccio@gmail.com>
2021-06-12 22:48:39 -0600

    Initial Commit with Code

---
+++ src/main/java/com/BoMC/API/API.java
@@ -0,0 +1,21 @@
+package com.BoMC.API;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class API extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		
+		
+	}
+
+}
